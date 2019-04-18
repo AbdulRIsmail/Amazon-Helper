871a2e981beba56924e8f579f584d12cf43e90ee
Abdul <abdul.ismail06@gmail.com>
2019-04-18 17:21:44 +0100

    creates collection div dynammically

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,95 +1,29 @@
-// global variables for price
-var ukPrice;
-var price;
-var priceDiv;
-var convertedPrice;
-var poundToEuroRate = 1.15674;
+const collectionDiv = document.getElementById('collection');
 
-// global variables for collections
-var collectionDiv;
-var collectionBtn;
+createCollectionClass = () => {
+    var collectionInfo = document.createElement('div');
+    collectionInfo.className = 'info';
 
-// amazon have two ways of displaying their price
-var tagsName = ['priceblock_dealprice', 'priceblock_ourprice'];
+    var infoTitle = document.createElement('p');
+    infoTitle.className = 'title';
+    infoTitle.innerText = 'hello';
 
-// displays the euro price underneath the uk price
-createEuroTag = (tag) => {
-    // create div
-    priceDiv = document.createElement('div');
-
-    // set the id for the div
-    priceDiv.id = 'priceInEuro';
-
-    // create a paragraph tag
-    convertedPrice = document.createElement('p');
-
-    // set the text of the paragraph 
-    convertedPrice.innerHTML = "Price In Euros: " + price;
+    var infoUkPrice = document.createElement('p');
+    infoUkPrice.className = 'ukPrice';
+    infoTitle.innerText = 'hello';
     
-    // append the div (price) underneath the actual price from amazon
-    document.getElementById(tag).appendChild(priceDiv);
+    var infoEURPrice = document.createElement('p');
+    infoEURPrice.className = 'eurPrice';
+    infoTitle.innerText = 'hello';
 
-    // append the paragraph (convertedPrice) inside the div (price)
-    document.getElementById(priceDiv.id).appendChild(convertedPrice);
+    document.getElementsByClassName(collectionInfo.className).appendChild(infoTitle);
+    document.getElementsByClassName(collectionInfo.className).appendChild(infoUkPrice);
+    document.getElementsByClassName(collectionInfo.className).appendChild(infoEURPrice);
+    document.getElementById('collection').appendChild(collectionInfo);
 }
 
-// formats the price
-formatPrice = (tag) => {
-    // create a temporary price variable and copy the price into it
-    price = document.getElementById(tag).innerHTML;
-
-    // split each character and convert into array
-    price = price.split('');
-
-    // remove the first element (£)
-    price.shift();
-
-    // join the array back together to form a string
-    price = price.join("");
-    ukPrice = price;
-}
-
-// converts the price
-poundToEuro = () => {
-    var tempPrice = poundToEuroRate * price;
-    price = tempPrice.toFixed(2);
-}
-
-// create button to add specifc items to collection
-createCollectionBtn = (tag) => {
-    // creates div 
-    collectionDiv = document.createElement('div');
-
-    // set the id for collectionDiv
-    collectionDiv.id = 'collection-amazonHelper';
-
-    // creates button
-    collectionBtn = document.createElement('button');
-
-    // change the text of the button
-    collectionBtn.innerHTML = 'Add To Collection';
-
-    // append the collectionDiv underneath the amazon price
-    document.getElementById(tag).appendChild(collectionDiv);
-
-    // append the button to the div
-    document.getElementById(collectionDiv.id).appendChild(collectionBtn);
-}
-
-tagsName.forEach((tag) => {
-    if (document.getElementById(tag)) {
-        formatPrice(tag);
-        poundToEuro();
-        createEuroTag(tag);
-        createCollectionBtn(tag);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('button').addEventListener('click', () => {
+        createCollectionClass();
+    });      
 });
-
-const collectionInfo = document.getElementById('collection-amazonHelper');
-
-collectionInfo.addEventListener('click', () => {
-    var title = document.querySelector('#title').innerText;
-    var image = document.querySelector('#landingImage').src;
-    var ukPrice = this.ukPrice;
-    var price = this.price;
-});      
---
+++ assets/js/price.js
@@ -0,0 +1,119 @@
+// global variables for price
+var ukPrice; // keeps the original price
+var price; // formats the original price - removes the £ sign
+var priceDiv; // div container for the price tag
+var convertedPrice; // euro price
+var poundToEuroRate = 1.15674; // the rate for 1 pound to euro
+
+// amazon have two ways of displaying their price
+var tagsName = ['priceblock_dealprice', 'priceblock_ourprice'];
+
+// formats the price
+formatPrice = (tag) => {
+    // create a temporary price variable and copy the price into it
+    price = document.getElementById(tag).innerHTML;
+
+    // split each character and convert into array
+    price = price.split('');
+
+    // remove the first element (£)
+    price.shift();
+
+    // join the array back together to form a string
+    price = price.join("");
+    ukPrice = price;
+}
+
+// converts the price
+poundToEuro = () => {
+    var tempPrice = poundToEuroRate * price;
+    price = tempPrice.toFixed(2);
+}
+
+// displays the euro price underneath the uk price
+createEuroTag = (tag) => {
+    // create div
+    priceDiv = document.createElement('div');
+
+    // set the id for the div
+    priceDiv.id = 'priceInEuro';
+
+    // create a paragraph tag
+    convertedPrice = document.createElement('p');
+
+    // set the text of the paragraph 
+    convertedPrice.innerHTML = "Price In Euros: €" + price;
+
+    // append the div (price) underneath the actual price from amazon
+    document.getElementById(tag).appendChild(priceDiv);
+
+    // append the paragraph (convertedPrice) inside the div (price)
+    document.getElementById(priceDiv.id).appendChild(convertedPrice);
+}
+
+createCollectionBtn = (tag) => {
+    // creates div 
+    collectionDiv = document.createElement('div');
+
+    // set the id for collectionDiv
+    collectionDiv.id = 'collection-amazonHelper';
+
+    // creates button
+    collectionBtn = document.createElement('button');
+
+    // set the id of the button
+    collectionBtn.id = 'collection-amazonHelper-button';
+
+    // change the text of the button
+    collectionBtn.innerHTML = 'Add To Collection';
+
+    // append the collectionDiv underneath the amazon price
+    document.getElementById(tag).appendChild(collectionDiv);
+
+    // append the button to the div
+    document.getElementById(collectionDiv.id).appendChild(collectionBtn);
+}
+
+tagsName.forEach((tag) => {
+    if (document.getElementById(tag)) {
+        formatPrice(tag);
+        poundToEuro();
+        createEuroTag(tag);
+        createCollectionBtn(tag);
+    }
+});
+
+// used to add product to the collection list
+createProduct = () => {    
+    // Collection Variables
+    var productInfo;
+    var productImg;
+    var productTitle;
+    var productPrice;
+
+    // creating the elements for each product
+    productInfo = document.createElement('div');
+    productImg = document.createElement('img');
+    productTitle = document.createElement('p');
+    productPrice = document.createElement('p');
+
+    // setting the class names
+    productInfo.className = 'productInfo';
+    productImg.className = 'productImg';
+    productTitle.className = 'productTitle';
+    productPrice.className = 'productPrice';
+
+    // appending all product info inside the productInfo div
+    productInfo.appendChild(productImg);
+    productInfo.appendChild(productTitle);
+    productInfo.appendChild(productPrice);
+
+    // appending the productInfo div to the collection div
+    document.getElementById('collection-amazonHelper').appendChild(productInfo);
+
+}
+
+// listens to if the collection button is clicked then 
+document.getElementById("collection-amazonHelper-button").addEventListener("click", () => {
+    createProduct();
+});
